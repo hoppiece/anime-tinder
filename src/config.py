6c0428bdd0a4401e15f83d7348eec254ba846e02
Aiwaka <Kw.I.KU.1414@gmail.com>
2021-08-31 15:54:06 +0900

    formatting

--- src/config.py
+++ src/config.py
@@ -1,20 +1,31 @@
-import os
-'''flaskのappのconfig設定オブジェクトを書く'''
+# 環境変数を使う場合はosをインポートする
+# import os
+
+"""flaskのappのconfig設定オブジェクトを書く"""
+
 
 class DevelopmentConfig:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{user}:{password}@{host}/{database}?charset=utf8mb4'.format(**{
-        #'user': os.getenv('DB_USER', 'root'),
-        'user': 'root',
-        #'password': os.getenv('DB_PASSWORD', 'root'),
-        'password': 'root',
-        # docker network で該当のものを探し, docker network inspect [entrypoint] で出てくる表示のContainersのNameがdbっぽいもののIPv4Addressを記述する. 現在はdocker-composeのnetwork設定により固定化している.
-        #'host': os.getenv('DB_HOST', '172.26.0.2'),
-        'host': '172.30.0.2',
-        #'database': os.getenv('DB_DATABASE', 'anime_recommender')
-        'database': 'anime_recommender'
-    })
+    SQLALCHEMY_DATABASE_URI = (
+        "mysql+pymysql://{user}:{password}" "@{host}/{database}?charset=utf8mb4"
+    ).format(
+        **{
+            # 'user': os.getenv('DB_USER', 'root'),
+            "user": "root",
+            # 'password': os.getenv('DB_PASSWORD', 'root'),
+            "password": "root",
+            # docker network で該当のものを探し,
+            # docker network inspect [entrypoint]
+            # で出てくる表示のContainersのNameがdbっぽいもののIPv4Addressを記述する.
+            # 現在はdocker-composeのnetwork設定により固定化している.
+            # 'host': os.getenv('DB_HOST', '172.26.0.2'),
+            "host": "172.30.0.2",
+            # 'database': os.getenv('DB_DATABASE', 'anime_recommender')
+            "database": "anime_recommender",
+        }
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = False
     JSON_AS_ASCII = False
+
 
 Config = DevelopmentConfig
--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,14 @@
-### 煩雑なツールをここに記述
+# 煩雑なツールをここに記述
 import os
 import base64
 
+
 def img_encode(img_name):
-    img_path = os.getcwd()+'/figures/'+img_name
+    img_path = os.getcwd() + "/figures/" + img_name
     if os.path.exists(img_path):
         with open(img_path, "rb") as img:
             data = base64.b64encode(img.read())
     else:
-        return 'not found'
+        return "not found"
 
-    return data.decode('utf-8')
+    return data.decode("utf-8")

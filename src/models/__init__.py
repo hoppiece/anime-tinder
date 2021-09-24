2e47c8f72cc0e6a81a3ac185e99dad1a5e982de1
Aiwaka <Kw.I.KU.1414@gmail.com>
2021-09-24 17:13:34 +0900

    for create_db()

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,5 +1,3 @@
 from .models import User, AnimeData, LikeUnlike, Recommended
 
-__all__ = [
-    'User', 'AnimeData', 'LikeUnlike', 'Recommended'
-]
+__all__ = [User, AnimeData, LikeUnlike, Recommended]
